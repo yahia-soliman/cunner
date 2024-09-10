22508a0fdce1a8663a6b1ecf305e4b0b35ab5105
Yahia <yn7599@gmail.com>
2024-09-10 14:55:05 +0300

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,9 @@ import tseslint from 'typescript-eslint';
 /** @type { import("eslint").Linter.Config } */
 export default [
   { files: ['**/*.ts'] },
-  { files: ['spec/**/*.js'],
-    languageOptions: { globals: globals.jasmine }
-  },
+  { files: ['spec/**/*.ts'], languageOptions: { globals: globals.jasmine } },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
 ];
